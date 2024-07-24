faa1d738af2118af2ee79136d88b1abf4a9e66eb
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-07-23 22:10:53 -0400

    Initial Commit

---
+++ KuviDesigniOS15/KuviDesigniOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  KuviDesigniOS15App.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 23-07-24.
+//
+
+import SwiftUI
+
+@main
+struct KuviDesigniOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
