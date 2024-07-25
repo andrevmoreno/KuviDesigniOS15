903e941ab15bfec29c50909a5c3ce28004cca72b
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-07-25 16:11:31 -0400

    Create AccountView

---
+++ KuviDesigniOS15/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 25-07-24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
