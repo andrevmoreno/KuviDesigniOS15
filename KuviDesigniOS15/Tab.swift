14742210da02f58194fd5cadd6dcaa338aead9ba
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-07-26 18:21:30 -0400

    Tab Bar Model

---
+++ KuviDesigniOS15/Tab.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 26-07-24.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Tab()
+}
---
+++ KuviDesigniOS15/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 26-07-24.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
