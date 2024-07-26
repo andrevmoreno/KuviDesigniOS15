451cc740d72afc0e271df75549137d8901afa8db
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-07-26 08:43:27 -0400

    Blog View

---
+++ KuviDesigniOS15/BlogView.swift
@@ -0,0 +1,18 @@
+//
+//  BlogView.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 25-07-24.
+//
+
+import SwiftUI
+
+struct BlogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlogView()
+}
---
+++ KuviDesigniOS15/HexagonView.swift
@@ -0,0 +1,18 @@
+//
+//  HexagonView.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 25-07-24.
+//
+
+import SwiftUI
+
+struct HexagonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HexagonView()
+}
