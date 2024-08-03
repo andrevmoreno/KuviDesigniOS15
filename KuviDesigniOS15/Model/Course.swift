a24de080d3904b62821901809be520fe8b199321
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-08-03 18:53:14 -0400

    Tab View Pagination

---
+++ KuviDesigniOS15/Model/Course.swift
@@ -0,0 +1,18 @@
+//
+//  Course.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 03-08-24.
+//
+
+import SwiftUI
+
+struct Course: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Course()
+}
---
+++ KuviDesigniOS15/Styles/AnimatableFontModifier.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatableFontModifier.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 03-08-24.
+//
+
+import SwiftUI
+
+struct AnimatableFontModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatableFontModifier()
+}
