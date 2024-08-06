3b123ea97952e43e2632cc4f8a1260c6faa36fb9
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-08-06 10:03:57 -0400

    Geometry Effect

---
+++ KuviDesigniOS15/Views/MatchedView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchedView.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 05-08-24.
+//
+
+import SwiftUI
+
+struct MatchedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchedView()
+}
