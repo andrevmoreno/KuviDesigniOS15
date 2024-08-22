63063c11fefa098b09bd2dc255fd5c7804a1f21a
andrevmoreno <andreavalentinamoreno158@gmail.com>
2024-08-21 20:01:10 -0400

    Searchable

---
+++ KuviDesigniOS15/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  KuviDesigniOS15
+//
+//  Created by Andrea Moreno on 21-08-24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
